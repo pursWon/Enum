b152818bf3cef55eaec203af060520491a0d8b69
Wongi <hpurs95@gmail.com>
2022-07-04 12:23:54 +0900

    Initial Commit

---
+++ enumeration/enumerationApp.swift
@@ -0,0 +1,17 @@
+//
+//  enumerationApp.swift
+//  enumeration
+//
+//  Created by MacBook Air on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct enumerationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
